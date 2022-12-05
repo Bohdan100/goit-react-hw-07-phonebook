519f094051521409b23b682abab133d2caf2b3a8
Bohdan <b.orlovskiy@gmail.com>
2022-12-05 02:10:43 +0200

    commit

--- src/redux/selectors/selectors.jsx
+++ src/redux/selectors/selectors.jsx
@@ -1,19 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit';
-
 export const selectContacts = state => state.contacts;
 
 export const selectFilter = state => state.filter;
-
-// export const selectVisibleContacts = createSelector(
-//   [selectContacts, selectFilter],
-//   (contacts, filterName) => {
-//     let visibleContacts = [];
-
-//     if (contacts) {
-//       visibleContacts = contacts.filter(contact =>
-//         contact.name.toLowerCase().includes(filterName)
-//       );
-//     }
-//     return visibleContacts;
-//   }
-// );
